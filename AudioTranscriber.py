e1a57c383d0b90ac8cf442e810f9d07f2e7d1959
InDate <joshua@shanksy.me>
2023-05-27 14:48:52 +0800

    wip: Changed audio model to use faster_whisper and made minor code formatting changes.

--- AudioTranscriber.py
+++ AudioTranscriber.py
@@ -1,4 +1,4 @@
-import whisper
+from faster_whisper import WhisperModel
 import torch
 import wave
 import os
@@ -7,18 +7,18 @@ from tempfile import NamedTemporaryFile
 import custom_speech_recognition as sr
 import io
 from datetime import timedelta
-import pyaudiowpatch as pyaudio
 from heapq import merge
 
 PHRASE_TIMEOUT = 3.05
 
 MAX_PHRASES = 10
 
+
 class AudioTranscriber:
     def __init__(self, mic_source, speaker_source):
         self.transcript_data = {"You": [], "Speaker": []}
         self.transcript_changed_event = threading.Event()
-        self.audio_model = whisper.load_model(os.path.join(os.getcwd(), 'tiny.en.pt'))
+        self.audio_model = WhisperModel(os.path.join(os.getcwd(), "tiny.en.pt"))
         self.audio_sources = {
             "You": {
                 "sample_rate": mic_source.SAMPLE_RATE,
@@ -27,7 +27,7 @@ class AudioTranscriber:
                 "last_sample": bytes(),
                 "last_spoken": None,
                 "new_phrase": True,
-                "process_data_func": self.process_mic_data
+                "process_data_func": self.process_mic_data,
             },
             "Speaker": {
                 "sample_rate": speaker_source.SAMPLE_RATE,
@@ -36,8 +36,8 @@ class AudioTranscriber:
                 "last_sample": bytes(),
                 "last_spoken": None,
                 "new_phrase": True,
-                "process_data_func": self.process_speaker_data
-            }
+                "process_data_func": self.process_speaker_data,
+            },
         }
 
     def transcribe_audio_queue(self, audio_queue):
@@ -48,32 +48,38 @@ class AudioTranscriber:
             temp_file = source_info["process_data_func"](source_info["last_sample"])
             text = self.get_transcription(temp_file)
 
-            if text != '' and text.lower() != 'you':
+            if text != "" and text.lower() != "you":
                 self.update_transcript(who_spoke, text, time_spoken)
                 self.transcript_changed_event.set()
 
     def update_last_sample_and_phrase_status(self, who_spoke, data, time_spoken):
         source_info = self.audio_sources[who_spoke]
-        if source_info["last_spoken"] and time_spoken - source_info["last_spoken"] > timedelta(seconds=PHRASE_TIMEOUT):
+        if source_info["last_spoken"] and time_spoken - source_info[
+            "last_spoken"
+        ] > timedelta(seconds=PHRASE_TIMEOUT):
             source_info["last_sample"] = bytes()
             source_info["new_phrase"] = True
         else:
             source_info["new_phrase"] = False
 
         source_info["last_sample"] += data
-        source_info["last_spoken"] = time_spoken 
+        source_info["last_spoken"] = time_spoken
 
     def process_mic_data(self, data):
         temp_file = NamedTemporaryFile().name
-        audio_data = sr.AudioData(data, self.audio_sources["You"]["sample_rate"], self.audio_sources["You"]["sample_width"])
+        audio_data = sr.AudioData(
+            data,
+            self.audio_sources["You"]["sample_rate"],
+            self.audio_sources["You"]["sample_width"],
+        )
         wav_data = io.BytesIO(audio_data.get_wav_data())
-        with open(temp_file, 'w+b') as f:
+        with open(temp_file, "w+b") as f:
             f.write(wav_data.read())
         return temp_file
 
     def process_speaker_data(self, data):
         temp_file = NamedTemporaryFile().name
-        with wave.open(temp_file, 'wb') as wf:
+        with wave.open(temp_file, "wb") as wf:
             wf.setnchannels(self.audio_sources["Speaker"]["channels"])
             p = pyaudio.PyAudio()
             wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
@@ -83,7 +89,7 @@ class AudioTranscriber:
 
     def get_transcription(self, file_path):
         result = self.audio_model.transcribe(file_path, fp16=torch.cuda.is_available())
-        return result['text'].strip()
+        return result["text"].strip()
 
     def update_transcript(self, who_spoke, text, time_spoken):
         source_info = self.audio_sources[who_spoke]
@@ -97,12 +103,17 @@ class AudioTranscriber:
             transcript[0] = (f"{who_spoke}: [{text}]\n\n", time_spoken)
 
     def get_transcript(self):
-        combined_transcript = list(merge(
-            self.transcript_data["You"], self.transcript_data["Speaker"], 
-            key=lambda x: x[1], reverse=True))
+        combined_transcript = list(
+            merge(
+                self.transcript_data["You"],
+                self.transcript_data["Speaker"],
+                key=lambda x: x[1],
+                reverse=True,
+            )
+        )
         combined_transcript = combined_transcript[:MAX_PHRASES]
         return "".join([t[0] for t in combined_transcript])
-    
+
     def clear_transcript_data(self):
         self.transcript_data["You"].clear()
         self.transcript_data["Speaker"].clear()
